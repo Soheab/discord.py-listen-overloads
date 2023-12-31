f1b8d983e8476da567a1be0b786f16618a899d73
Soheab_ <33902984+Soheab@users.noreply.github.com>
2023-12-31 16:10:17 +0100

    Add all stubs

---
+++ listen_overloads/discord/ext/commands/events_overloads/app_commands.py
@@ -0,0 +1,18 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING, Any, Protocol
+
+if TYPE_CHECKING:
+    from discord import RawAppCommandPermissionsUpdateEvent, Interaction
+
+__all__ = (
+    "RawApplicationCommandPermissionsUpdateEvent",
+    "ApplicationCommandCompletionEvent",
+)
+
+# on_raw_app_command_permissions_update
+class RawApplicationCommandPermissionsUpdateEvent(Protocol):
+    async def __call__(self, payload: RawAppCommandPermissionsUpdateEvent) -> Any: ...
+
+# on_app_command_completion
+class ApplicationCommandCompletionEvent(Protocol):
+    async def __call__(self, interaction: Interaction[Any]) -> Any: ...
--- listen_overloads/typings/discord/ext/commands/__init__.pyi
+++
@@ -1,2 +0,0 @@
-from .bot import AutoShardedBot as AutoShardedBot  # noqa: F401
-from .bot import Bot as Bot  # noqa: F401
